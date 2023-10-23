bf69b409e52218637bddbc0ab19848395aecfdd5
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-23 19:19:05 -0300

    05-Actions and reducers

--- src/store/movies.js
+++ src/store/movies.js
@@ -7,13 +7,15 @@ export const moviesSlice = createSlice({
         list:[
             {id:1,title:'Pulp fiction'},
             {id:2,title:'Rambo'}
-        ],
-        name:'sadasd',
-        date:'saasf'
+        ]
     },
     reducers:{
-
+        addMovie:(state,action)=>{
+            // const newMovie = {id:3,title:'Batman'};
+            state.list = [...state.list,action.payload]
+        }
     }
 });
 
+export const { addMovie } = moviesSlice.actions;
 export default moviesSlice.reducer;
